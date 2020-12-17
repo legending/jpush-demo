c24d30b48097f5a954e256d95bd213ed893d1d21
Robin Wang <510471194@qq.com>
2020-12-17 17:59:39 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+- 官方client示例：https://github.com/jpush/jpush-api-java-client
+- 官方Android示例：https://github.com/jpush/jpush-android-samples
---
+++ src/main/java/com/legend/jpushdemo/JpushDemoApplication.java
@@ -0,0 +1,13 @@
+package com.legend.jpushdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpushDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpushDemoApplication.class, args);
+    }
+
+}
